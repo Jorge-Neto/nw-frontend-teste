da1e68779789f65e5be9f8f0e6483ae6ecdf4d40
Jorge.Neto <jorge.neto@mirumagency.com>
2022-04-13 09:11:18 -0300

    Andamento na versão mobile

--- assets/js/swiper/swiper.js
+++ assets/js/swiper/swiper.js
@@ -1,6 +1,27 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerview: 9,
-    spaceBetween: 40,
-    speed: 2,
-    slidesPerGroup: 3
-  });
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 9,
+  centerInsufficientSlides: true,
+  spaceBetween: 25,
+  slidesPerGroup: 1,
+  loopFillGroupWithBlank: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    hide: false
+  },
+  breakpoints: {
+    // when window width is >= 1024px
+    1024: {
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      spaceBetween: 15
+    },
+    // when window width is >= 640px
+    375: {
+      slidesPerView: 2.5,
+      spaceBetween: 15
+    }
+  }
+});
